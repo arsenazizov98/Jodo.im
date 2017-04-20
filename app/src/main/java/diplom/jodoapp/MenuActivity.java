2e61025db9b7aa479084e84ef0b56db35713e662
Арсенсан Азизовджан < >
2017-04-20 23:46:13 +0300

    therd_commit_switch_colors

--- app/src/main/java/diplom/jodoapp/MenuActivity.java
+++ app/src/main/java/diplom/jodoapp/MenuActivity.java
@@ -1,13 +1,38 @@
 package diplom.jodoapp;
 
+import android.graphics.Color;
+import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
 
 public class MenuActivity extends AppCompatActivity {
 
+    private Switch mySwitch;
+    private boolean bSwitch = true;
+    private CoordinatorLayout menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        mySwitch = (Switch) findViewById(R.id.switch1);
+        menu = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+
+        mySwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bSwitch) {
+                    menu.setBackgroundColor(Color.parseColor("#FBC711"));
+                    bSwitch=false;
+                }
+                else{
+
+                    menu.setBackgroundColor(Color.parseColor("#45B735"));
+                    bSwitch=true;
+                }
+            }
+        });
+
     }
 }
